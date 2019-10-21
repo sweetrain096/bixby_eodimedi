7c69f9d7b719076ba80a4952ff8a42418952871d
Park Sangho <sxngho@naver.com>
2019-10-21 13:41:35 +0900

    [#6] S1P2251003-23 상세페이지 비즈니르로직

--- playground.eodiyak/code/getHospitalInfo.js
+++ playground.eodiyak/code/getHospitalInfo.js
@@ -1,16 +1,48 @@
+var http = require('http')
+
+var EndPoint = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/"
+var Operation = "getHsptlBassInfoInqire"
+var ServiceKey = "Z6lJuu3urgG5yS0Gsn67Vc7jF4RBEpoMneik3qshCxF%2FoQDSri4aC8TThqkniotYQ%2Flgpc23f6ByJ6Sp0uPvBw%3D%3D"
+
+var treatmentList = new Array(
+  "내과","소아청소년과","신경과","정신건강의학과","피부과",
+  "외과","흉부외과","정형외과","신경외과","성형외과",
+  "산부인과","안과","이비인후과","비뇨기과","재활의학과",
+  "마취통증의학과","영상의학과","치료방사선과","임상병리과","해부병리과",
+  "가정의학과","핵의학과","응급의학과","치과","구강악안면외과"
+)
+
 module.exports.function = function getHospitalInfo (hpId) {
-  let name, tel, addr, endtime;
-  let result = {}
-  name = "A병원"
-  tel = "032-111-1111"
-  addr = "대전 유성구 어쩌구저쩌구"
-  endtime = "0630"
+  const console = require("console")
+  
+  var url = EndPoint + Operation 
+  + "?ServiceKey=" + ServiceKey 
+  + "&HPID=" + hpId
+  var details = http.getUrl(url,{format: 'xmljs'})
 
-  result['name'] = name
-  result['addr'] = addr
-  result['dutytel'] = tel
-  result['endtime'] = endtime
+  var item = details.response.body.items.item
+
+  var res = "true,false,true,false"
+  var dgidldList = new Array();
+  for(var i=0; i<treatmentList.length; i++){
+    if(item.dgidIdName.includes(treatmentList[i])){
+        dgidldList[i] = true
+    }else{
+        dgidldList[i] = false
+    }
+  }
+  
+
+  let info = {}
+  info['wgs84Lat'] = item.wgs84Lat
+  info['wgs84Lon'] = item.wgs84Lon
+  info['dutyAddr'] = item.dutyAddr
+  info['dutyName'] = item.dutyName
+  info['dgidIdName'] = dgidldList
+  info['dutyTel1'] = item.dutyTel1
+  // info['startTime'] = startTime
+  // info['endTime'] = endTime
 
 
-  return result
+  return info
 }
