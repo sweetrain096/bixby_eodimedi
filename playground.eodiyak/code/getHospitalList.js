be7b5362d79f66d58c7e31ef3fb64ead5fc1f1a2
Park Sangho <sxngho@naver.com>
2019-10-22 17:37:36 +0900

    [#6] S1P2251003-28 달빛병원 비즈니스 로직 구현

--- playground.eodiyak/code/getHospitalList.js
+++ playground.eodiyak/code/getHospitalList.js
@@ -1,28 +1,30 @@
 var http = require('http')
 var EndPoint = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/"
 var Operation = "getHsptlMdcncLcinfoInqire"
+var BabyOperation = "getBabyLcinfoInqire"
 var ServiceKey = "Z6lJuu3urgG5yS0Gsn67Vc7jF4RBEpoMneik3qshCxF%2FoQDSri4aC8TThqkniotYQ%2Flgpc23f6ByJ6Sp0uPvBw%3D%3D"
 var pageNo = 1
 var num = 50
 
 module.exports.function = function getHospitalList (position, baby) {
   const console = require("console")
-  console.log(baby)
+  var url = ""
   if (baby == true){
-    console.log("달빛병원이에요!!")
-
+    url = EndPoint + BabyOperation 
+    + "?ServiceKey=" + ServiceKey 
+    + "&WGS84_LON=" + position['myPos']['longitude']
+    + "&WGS84_LAT=" + position['myPos']['latitude']
+    + "&pageNo=" + pageNo
+    + "&numOfRows=" + num
+  } else {
+    url = EndPoint + Operation 
+    + "?ServiceKey=" + ServiceKey 
+    + "&WGS84_LON=" + position['myPos']['longitude']
+    + "&WGS84_LAT=" + position['myPos']['latitude']
+    + "&pageNo=" + pageNo
+    + "&numOfRows=" + num
   }
   let results = new Array
-    
-    // position['myPos']['latitude'] // 위도 
-    // position['myPos']['longitude'] // 경도
-
-  var url = EndPoint + Operation 
-  + "?ServiceKey=" + ServiceKey 
-  + "&WGS84_LON=" + position['myPos']['longitude']
-  + "&WGS84_LAT=" + position['myPos']['latitude']
-  + "&pageNo=" + pageNo
-  + "&numOfRows=" + num
 
   var hList = http.getUrl(url,{format: 'xmljs'})
   var item = hList.response.body.items.item
