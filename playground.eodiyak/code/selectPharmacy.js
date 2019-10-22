22a86aefea9041ce960e45e91b95d38f862fcae1
sweetrain096 <gpfhddl09@gmail.com>
2019-10-22 15:02:30 +0900

    [#4] S1P2251003-17 달빛병원 action 추가

--- playground.eodiyak/code/selectPharmacy.js
+++ playground.eodiyak/code/selectPharmacy.js
@@ -1,10 +1,12 @@
 module.exports.function = function selectPharmacy (pharmacy) {
   const console = require("console")
-  console.log(pharmacy)
+  console.log(pharmacy.pHpid[0])
+  console.log(pharmacy.pStartTime[0])
+  console.log(pharmacy.pEndTime[0])
   return {
-    hpId: pharmacy.hpid,
-    startTime: pharmacy.startTime,
-    endTime: pharmacy.endTime
+    pHpId: pharmacy.pHpid[0],
+    pStartTime: pharmacy.pStartTime[0],
+    pEndTime: pharmacy.pEndTime[0]
   }
 }
  
