5398e5a4b2051994a53e2c8c789df063ba809fe9
Park Sangho <sxngho@naver.com>
2019-10-23 15:03:50 +0900

    [#6] S1P2251003-23 검색 결과 0개 예외처리

--- playground.eodiyak/code/getHospitalInfo.js
+++ playground.eodiyak/code/getHospitalInfo.js
@@ -2,62 +2,76 @@ var http = require('http')
 
 var EndPoint = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/"
 var Operation = "getHsptlBassInfoInqire"
-var ServiceKey = "K6sYWvqebVyngpczytPk5eOtSHyZapagbhcTDE31E6hsk57L5V8cJdQKn033Fvj4QU7m6jfDg7evWZLIgCHPBw%3D%3D"
+var ServiceKey = "Z6lJuu3urgG5yS0Gsn67Vc7jF4RBEpoMneik3qshCxF%2FoQDSri4aC8TThqkniotYQ%2Flgpc23f6ByJ6Sp0uPvBw%3D%3D"
 
 var treatmentList = new Array(
-  "내과", "소아청소년", "이비인후과",
-  "안과", "치과", "피부과",
-  "가정의학과", "산부인과", "비뇨기과",
-  "외과", "정형외과", "성형외과",
-  "흉부외과", "재활의학과", "응급의학과",
-  "정신건강의학과", "신경과", "신경외과",
-  "영상의학과", "치료방사선과", "핵의학과",
-  "해부병리과", "임상병리과",
-  "마취통증의학과", "구강안면외과"
+  "내과","소아청소년과","신경과","정신건강의학과","피부과",
+  "외과","흉부외과","정형외과","신경외과","성형외과",
+  "산부인과","안과","이비인후과","비뇨기과","재활의학과",
+  "마취통증의학과","영상의학과","치료방사선과","임상병리과","해부병리과",
+  "가정의학과","핵의학과","응급의학과","치과","구강악안면외과"
 )
 
 module.exports.function = function getHospitalInfo (hospitalSummaryInfo,currentPosition) {
   const console = require("console")
-  var url = EndPoint + Operation 
-  + "?ServiceKey=" + ServiceKey 
-  + "&HPID=" + hospitalSummaryInfo.hpId
-  var details = http.getUrl(url,{format: 'xmljs'})
+  
+  let info = {}
+  if ( hospitalSummaryInfo.hpId == "null" && hospitalSummaryInfo.startTime == "null" && hospitalSummaryInfo.endTime == "null" ) {
+    info['point'] = {
+      latitude : null,
+      longitude : null,
+      $id : null,
+      $type : "viv.geo.GeoPoint"
+    }
+    info['dutyAddr'] = null
+    info['dutyName'] = null
+    info['dgidIdName'] = null
+    info['dutyTel1'] = null
+    info['startTime'] = null
+    info['endTime'] = null
+    info['currentPosition'] = null
+    info['url'] = null
+    return info
+  } else {
+    var url = EndPoint + Operation 
+    + "?ServiceKey=" + ServiceKey 
+    + "&HPID=" + hospitalSummaryInfo.hpId
+    var details = http.getUrl(url,{format: 'xmljs'})
 
-  var item = details.response.body.items.item
+    var item = details.response.body.items.item
 
-  var dgidldList = new Array();
-  var originDNList = item.dgidIdName.split(",");
+    var dgidldList = new Array();
+    var originDNList = item.dgidIdName.split(",");
 
-  for(var i=0; i<treatmentList.length; i++){
+    for(var i=0; i<treatmentList.length; i++){
 
-    if(item.dutyName.includes(treatmentList[i])){
-      dgidldList[i] = true;
-    }else{
-      for(var j=0; j<originDNList.length; j++){
-        if(treatmentList[i] == originDNList[j]){
-          dgidldList[i] = true;
-        }else{
-          dgidldList[i] = false;
+      if(item.dutyName.includes(treatmentList[i])){
+        dgidldList[i] = true;
+      }else{
+        for(var j=0; j<originDNList.length; j++){
+          if(treatmentList[i] == originDNList[j]){
+            dgidldList[i] = true;
+          }else{
+            dgidldList[i] = false;
+          }
         }
       }
     }
+    info['point'] = {
+      latitude : item.wgs84Lat,
+      longitude : item.wgs84Lon,
+      $id : null,
+      $type : "viv.geo.GeoPoint"
+    }
+    info['dutyAddr'] = item.dutyAddr
+    info['dutyName'] = item.dutyName
+    info['dgidIdName'] = dgidldList
+    info['dutyTel1'] = item.dutyTel1
+    info['startTime'] = hospitalSummaryInfo.startTime
+    info['endTime'] = hospitalSummaryInfo.endTime
+    info['currentPosition'] = currentPosition
+    info['url'] = 'https://search.naver.com/search.naver?query=' + info['dutyName']
   }
-  let info = {}
-  info['point'] = {
-    latitude : item.wgs84Lat,
-    longitude : item.wgs84Lon,
-    $id : null,
-    $type : "viv.geo.GeoPoint"
-  }
-  info['dutyAddr'] = item.dutyAddr
-  info['dutyName'] = item.dutyName
-  info['dgidIdName'] = dgidldList
-  info['dutyTel1'] = item.dutyTel1
-  info['startTime'] = hospitalSummaryInfo.startTime
-  info['endTime'] = hospitalSummaryInfo.endTime
-  info['currentPosition'] = currentPosition
-  info['url'] = 'https://search.naver.com/search.naver?query=' + info['dutyName']
-
-  return info
+    return info
 }
 
--- playground.eodiyak/code/getPharmacyInfo.js
+++ playground.eodiyak/code/getPharmacyInfo.js
@@ -2,37 +2,43 @@ var http = require('http')
 
 var EndPoint = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/"
 var Operation = "getParmacyBassInfoInqire"
-var ServiceKey = "K6sYWvqebVyngpczytPk5eOtSHyZapagbhcTDE31E6hsk57L5V8cJdQKn033Fvj4QU7m6jfDg7evWZLIgCHPBw%3D%3D"
+var ServiceKey = "Z6lJuu3urgG5yS0Gsn67Vc7jF4RBEpoMneik3qshCxF%2FoQDSri4aC8TThqkniotYQ%2Flgpc23f6ByJ6Sp0uPvBw%3D%3D"
 
 module.exports.function = function getPharmacyInfo (pharmacySummaryInfo,currentPosition) {
   const console = require("console")
+  let info = {}
+  if ( pharmacySummaryInfo.pHpId == "null" && pharmacySummaryInfo.pStartTime == "null" && pharmacySummaryInfo.pEndTime == "null" ) {
+      info['point'] = null
+      info['pDutyAddr'] = null
+      info['pDutyName'] = null
+      info['pDutyTel1'] = null
+      info['pStartTime'] = null
+      info['pEndTime'] = null
+      info['currentPosition'] = null
+      info['purl'] = null
+  } else {
+    var url = EndPoint + Operation 
+      + "?ServiceKey=" + ServiceKey 
+      + "&HPID=" + pharmacySummaryInfo.pHpId
 
-  // HPID=C1601135
-  var url = EndPoint + Operation 
-    + "?ServiceKey=" + ServiceKey 
-    + "&HPID=" + pharmacySummaryInfo.pHpId
+      var details = http.getUrl(url,{format: 'xmljs'})
 
-    var details = http.getUrl(url,{format: 'xmljs'})
-
-    var item = details.response.body.items.item
-    console.log(details)
-    let info = {}
-    // info['pWgs84Lat'] = item.wgs84Lat
-    // info['pWgs84Lon'] = item.wgs84Lon
-    info['point'] = {
-    latitude : item.wgs84Lat,
-    longitude : item.wgs84Lon,
-    $id : null,
-    $type : "viv.geo.GeoPoint"
-  }
-    
-    info['pDutyAddr'] = item.dutyAddr
-    info['pDutyName'] = item.dutyName
-    info['pDutyTel1'] = item.dutyTel1
-    info['pStartTime'] = pharmacySummaryInfo.pStartTime
-    info['pEndTime'] = pharmacySummaryInfo.pEndTime
-    info['currentPosition'] = currentPosition
-    info['purl'] = 'https://search.naver.com/search.naver?query=' + info['pDutyName']
-
+      var item = details.response.body.items.item
+  
+      info['point'] = {
+      latitude : item.wgs84Lat,
+      longitude : item.wgs84Lon,
+      $id : null,
+      $type : "viv.geo.GeoPoint"
+    }
+      
+      info['pDutyAddr'] = item.dutyAddr
+      info['pDutyName'] = item.dutyName
+      info['pDutyTel1'] = item.dutyTel1
+      info['pStartTime'] = pharmacySummaryInfo.pStartTime
+      info['pEndTime'] = pharmacySummaryInfo.pEndTime
+      info['currentPosition'] = currentPosition
+      info['purl'] = 'https://search.naver.com/search.naver?query=' + info['pDutyName']
+    }
     return info
 }
