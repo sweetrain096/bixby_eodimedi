5b04b6b9c79df0c5e092a4c95e203d75f15dfcbf
min <smp1991@naver.com>
2019-11-01 13:04:52 +0900

    [#12] S1P2251003-42 detail view 생성

---
+++ vue_cli/src/views/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <Detail/>
+</template>
+
+<script>
+import Detail from '@/components/Detail/Detail.vue'
+
+export default {
+    components: {
+        Detail
+    }
+}
+</script>
+
+<style>
+
+</style>
