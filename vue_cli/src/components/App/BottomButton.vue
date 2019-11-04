8f12b1194bc5624ae027fbd1ad3e48e34e63c4ea
min <smp1991@naver.com>
2019-11-04 11:07:56 +0900

    [#16] S1P2251003-47 진료과목 버튼 클릭 시 페이지 전환 설정

--- vue_cli/src/components/App/BottomButton.vue
+++ vue_cli/src/components/App/BottomButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="bottom">
-    <button class="button">
+    <button class="button" >
       <i class="fas fa-search icon"></i>병원 찾기
     </button>
     <button class="button">
@@ -9,20 +9,34 @@
     <button class="button">
       <i class="fas fa-search icon"></i>진료 과목
     </button>
-    <button class="button">
-      <i class="fas fa-search icon"></i>달빛 병원
-    </button>
+    <router-link to="/subject" style="text-decoration: none !important;">
+      <button class="button">
+        <i class="fas fa-search icon"></i>달빛 병원
+      </button>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BottomButton'
+  name: 'BottomButton',
+  methods: {
+    goToHospitalSubject: function() {    
+    }
+  },
 }
 </script>
 
 <style>
 #bottom {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  background-color: #10BECB;
+}
+#a {
   position: fixed;
   bottom: 0;
   left: 0;
