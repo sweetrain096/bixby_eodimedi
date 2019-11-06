b8b4277e45b60a71cb98cf5e3f1f824ac8f2235b
silverlyjoo <silverlyjoo@gmail.com>
2019-11-06 09:56:58 +0900

    [#16] about

--- eodimedi/src/router.js
+++ eodimedi/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue'
+import Main from '@/views/Main.vue'
 import Detail from '@/views/Detail.vue'
 import About from '@/views/About.vue'
 import SelectHospitalSubject from '@/views/SelectHospitalSubject.vue'
@@ -11,8 +12,7 @@ export default new Router({
     mode: 'history',
     routes: [{
             path: '/',
-            name: 'home',
-            component: Home
+            redirect: '/search/hospital'
         },
         {
             path: '/about',
--- eodimedi/src/views/About.vue
+++ eodimedi/src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+      <span>
+        &copy;2019 —
+        <strong>Team TODOC</strong>
+      </span><br><br>
+      <span>Contact US : <a class="mail" href="mailto:todoc119@gmail.com">todoc119@gmail.com</a></span>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    setHeight() {
+      // let wHeight = window.screen.height;
+      // document.getElementById("about").style.height = wHeight - 149 + "px";
+    }
+  },
+  mounted() {
+    this.setHeight();
+  }
+};
+</script>
+
+<style scoped>
+#about {
+  display: block;
+  bottom: 90px;
+  margin-top: 300px;;
+}
+.mail {
+  text-decoration: none;
+  color: darkgrey;
+}
+</style>
