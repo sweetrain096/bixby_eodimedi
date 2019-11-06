cdbc8bbefe335347dd4da33ef9072e8335e9967c
min <smp1991@naver.com>
2019-11-06 10:06:30 +0900

    [#16] S1P2251003-47 pdetail router 설정

--- eodimedi/src/router.js
+++ eodimedi/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue'
 import Detail from '@/views/Detail.vue'
+import Pdetail from '@/views/Pdetail.vue'
 import SelectHospitalSubject from '@/views/SelectHospitalSubject.vue'
 
 Vue.use(Router)
@@ -17,6 +18,12 @@ export default new Router({
             path: '/detail',
             name: 'detail',
             component: Detail,
+            props: true
+        },
+        {
+            path: '/pdetail',
+            name: 'pdetail',
+            component: Pdetail,
             props: true
         },
         {
